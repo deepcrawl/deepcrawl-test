ecfd82f36fc6df4d11e03c35956ca0a7b0b3f200
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2021-09-29 15:32:35 +0100

    fix

--- packages/test-cli/src/index.ts
+++ packages/test-cli/src/index.ts
@@ -1,17 +1,46 @@
 import { TestSDKClient } from "@deepcrawl/test-nodejs-sdk";
+import { parse } from "ts-command-line-args";
 
-console.log("test");
+interface IRunBuildArguments {
+  testSuiteId: string;
+  userKeyId?: string;
+  userKeySecret?: string;
+  ciBuildId?: string;
+  startOnly?: boolean;
+  help?: boolean;
+}
 
 (async (): Promise<void> => {
-  const [, , ...args] = process.argv;
-  const userKeyId = args[0];
-  const userKeySecret = args[1];
+  const args = parse<IRunBuildArguments>(
+    {
+      testSuiteId: { type: String, description: "The test suite id." },
+      userKeyId: {
+        type: String,
+        optional: true,
+        description: "The user key id, which can be provided as env variable DEEPCRAWL_TEST_USER_KEY_ID.",
+      },
+      userKeySecret: {
+        type: String,
+        optional: true,
+        description: "The user key secret, which can be provided as env variable DEEPCRAWL_TEST_USER_KEY_SECRET.",
+      },
+      ciBuildId: { type: String, optional: true, description: "The corresponding CI Build ID." },
+      startOnly: {
+        type: Boolean,
+        optional: true,
+        description: "Only starts the build without waiting for it to finish.",
+      },
+      help: { type: Boolean, optional: true, alias: "h", description: "Prints this usage guide." },
+    },
+    {
+      helpArg: "help",
+    },
+  );
+  const userKeyId = args.userKeyId ?? process.env.DEEPCRAWL_TEST_USER_KEY_ID ?? "";
+  const userKeySecret = args.userKeySecret ?? process.env.DEEPCRAWL_TEST_USER_KEY_SECRET ?? "";
   const testSDKClient = TestSDKClient.create();
-  await testSDKClient.runBuild({ userKeyId, userKeySecret, testSuiteId: args[2] });
+  const { testSuiteId, ciBuildId, startOnly: isStartOnly } = args;
+  await testSDKClient.runBuild({ userKeyId, userKeySecret, testSuiteId, ciBuildId, isStartOnly });
 })()
-  .then(() => {
-    console.log("done");
-  })
-  .catch(e => {
-    console.log(e);
-  });
+  .then(() => {})
+  .catch(console.log);
