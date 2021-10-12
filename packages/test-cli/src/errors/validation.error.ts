a0d43a16b651eecb6d0219377f72ac561b8167b0
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2021-10-12 17:36:22 +0100

    fix

---
+++ packages/test-cli/src/errors/validation.error.ts
@@ -0,0 +1,7 @@
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
--- packages/test-cli/src/index.ts
+++ packages/test-cli/src/index.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
 import { TestSDKClient } from "@deepcrawl/test-nodejs-sdk";
 import { parse } from "ts-command-line-args";
+
+import { ValidationError } from "./errors/validation.error";
 
 interface IRunBuildArguments {
   testSuiteId: string;
@@ -11,50 +14,46 @@ interface IRunBuildArguments {
 }
 
 function validateArgs({ testSuiteId, userKeyId, userKeySecret }: IRunBuildArguments): void {
-  if (testSuiteId === "") throw new Error("--testSuiteId cannot be empty.");
-  if (userKeyId === "") throw new Error("--userKeyId cannot be empty.");
-  if (userKeySecret === "") throw new Error("--userKeySecret cannot be empty.");
+  if (testSuiteId === "") throw new ValidationError("--testSuiteId cannot be empty.");
+  if (userKeyId === "") throw new ValidationError("--userKeyId cannot be empty.");
+  if (userKeySecret === "") throw new ValidationError("--userKeySecret cannot be empty.");
 }
 
 // eslint-disable-next-line max-lines-per-function
 (async (): Promise<void> => {
-  try {
-    const args = parse<IRunBuildArguments>(
-      {
-        testSuiteId: { type: String, description: "The test suite ID." },
-        userKeyId: {
-          type: String,
-          defaultValue: process.env.DEEPCRAWL_TEST_USER_KEY_ID,
-          description: "The user key ID, which can also be provided as env variable DEEPCRAWL_TEST_USER_KEY_ID.",
-        },
-        userKeySecret: {
-          type: String,
-          defaultValue: process.env.DEEPCRAWL_TEST_USER_KEY_SECRET,
-          description:
-            "The user key secret, which can also be provided as env variable DEEPCRAWL_TEST_USER_KEY_SECRET.",
-        },
-        ciBuildId: { type: String, optional: true, description: "The corresponding build ID in your CI/CD pipeline." },
-        startOnly: {
-          type: Boolean,
-          optional: true,
-          description: "Flags if should only start the build without waiting for it to finish.",
-        },
-        help: { type: Boolean, optional: true, alias: "h", description: "Prints the usage guide." },
+  const args = parse<IRunBuildArguments>(
+    {
+      testSuiteId: { type: String, description: "The test suite ID." },
+      userKeyId: {
+        type: String,
+        defaultValue: process.env.DEEPCRAWL_TEST_USER_KEY_ID,
+        description: "The user key ID, which can also be provided as env variable DEEPCRAWL_TEST_USER_KEY_ID.",
       },
-      {
-        helpArg: "help",
+      userKeySecret: {
+        type: String,
+        defaultValue: process.env.DEEPCRAWL_TEST_USER_KEY_SECRET,
+        description: "The user key secret, which can also be provided as env variable DEEPCRAWL_TEST_USER_KEY_SECRET.",
       },
-    );
-    validateArgs(args);
-    const testSDKClient = TestSDKClient.create();
-    await testSDKClient.runBuild({ ...args, isStartOnly: args.startOnly });
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.log(e.message);
-  }
+      ciBuildId: { type: String, optional: true, description: "The corresponding build ID in your CI/CD pipeline." },
+      startOnly: {
+        type: Boolean,
+        optional: true,
+        description: "Flags if should only start the build without waiting for it to finish.",
+      },
+      help: { type: Boolean, optional: true, alias: "h", description: "Prints the usage guide." },
+    },
+    {
+      helpArg: "help",
+    },
+  );
+  validateArgs(args);
+  const testSDKClient = TestSDKClient.create();
+  await testSDKClient.runBuild({ ...args, isStartOnly: args.startOnly });
 })()
   .then(() => {
     return;
   })
-  // eslint-disable-next-line no-console
-  .catch(console.log);
+  .catch(e => {
+    if (e instanceof ValidationError) console.log(e.message);
+    console.log(e);
+  });
